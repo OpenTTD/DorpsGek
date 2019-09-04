77bd893e6aaed2513bb67e897a135955473581e2
Patric Stout <truebrain@openttd.org>
2019-09-04 19:35:00 +0200

    Add: also track commit-comment events
    This hadn't happen before, but is useful to track.

---
+++ dorpsgek/events/commit_comment.py
@@ -0,0 +1,26 @@
+from dorpsgek.helpers.github import router
+from dorpsgek.helpers import protocols
+
+
+@router.register("commit_comment")
+async def commit_comment(event, github_api):
+    repository_name = event.data["repository"]["full_name"]
+
+    if event.data["action"] != "created":
+        return
+
+    commits_url = event.data["repository"]["commits_url"]
+    assert commits_url.startswith("https://api.github.com")
+    commits_url = commits_url[len("https://api.github.com"):]
+    assert commits_url.endswith("{/sha}")
+    commits_url = commits_url.replace("{/sha}", "/" + event.data["comment"]["commit_id"])
+    response = await github_api.getitem(commits_url)
+
+    payload = {
+        "repository_name": repository_name,
+        "url": event.data["comment"]["html_url"],
+        "user": event.data["sender"]["login"],
+        "message": response["commit"]["message"].split("\n")[0],
+    }
+
+    await protocols.dispatch(github_api, repository_name, "commit-comment", payload)
