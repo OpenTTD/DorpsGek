f4e5a1fa0bdd553075e2ba0a7bcafd1b80714537
Patric Stout <truebrain@openttd.org>
2020-10-05 12:21:47 +0200

    Codechange: use openttd-helpers library for common functions (#37)
    * Change: switch from Python 3.7 to Python 3.8
    
    * Codechange: use openttd-helpers library for common functions

--- dorpsgek/__main__.py
+++ dorpsgek/__main__.py
@@ -4,11 +4,12 @@ import logging
 
 from aiohttp import web
 from aiohttp.web_log import AccessLogger
+from openttd_helpers import click_helper
+from openttd_helpers.logging_helper import click_logging
+from openttd_helpers.sentry_helper import click_sentry
+
 from dorpsgek import web_routes
-from dorpsgek.helpers import (
-    github,
-    sentry,
-)
+from dorpsgek.helpers import github
 
 # Import files that will hook themself up when imported
 from dorpsgek.events import (  # noqa
@@ -23,8 +24,6 @@ from dorpsgek.protocols import irc  # noqa
 
 log = logging.getLogger(__name__)
 
-CONTEXT_SETTINGS = {"help_option_names": ["-h", "--help"]}
-
 
 class ErrorOnlyAccessLogger(AccessLogger):
     def log(self, request, response, time):
@@ -33,7 +32,9 @@ class ErrorOnlyAccessLogger(AccessLogger):
             super().log(request, response, time)
 
 
-@click.command(context_settings=CONTEXT_SETTINGS)
+@click_helper.command()
+@click_logging  # Should always be on top, as it initializes the logging
+@click_sentry
 @click.option("--github-app-id", help="GitHub App ID")
 @click.option("--github-app-private-key", help="GitHub App Private Key")
 @click.option(
@@ -43,29 +44,13 @@ class ErrorOnlyAccessLogger(AccessLogger):
 )
 @click.option("--github-app-secret", help="GitHub App Secret", required=True)
 @click.option("--port", help="Port of the server", default=80, show_default=True)
-@click.option("--sentry-dsn", help="Sentry DSN")
-@click.option(
-    "--sentry-environment",
-    help="Environment we are running in (for Sentry)",
-    default="development",
-)
 def main(
     github_app_id,
     github_app_private_key,
     github_app_private_key_file,
     github_app_secret,
     port,
-    sentry_dsn,
-    sentry_environment,
 ):
-    sentry.setup_sentry(sentry_dsn, sentry_environment)
-
-    logging.basicConfig(
-        format="%(asctime)s %(levelname)-8s %(message)s",
-        datefmt="%Y-%m-%d %H:%M:%S",
-        level=logging.INFO,
-    )
-
     github.GITHUB_APP_SECRET = github_app_secret
     github.GITHUB_APP_ID = github_app_id
     if github_app_private_key_file:
