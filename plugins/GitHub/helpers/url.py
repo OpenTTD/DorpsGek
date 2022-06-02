b7201da6562e3be86dd42c182dae55dee11ca27b
Patric Stout <truebrain@openttd.org>
2022-06-02 08:25:35 +0000

    Remove: git.io closed its doors; no longer try to use their service
    There also really isn't an alternative, so we will have to make do
    with the very long URLs.

--- plugins/GitHub/helpers/url.py
+++ plugins/GitHub/helpers/url.py
@@ -1,13 +1,2 @@
-import aiohttp
-
-
 async def shorten(url):
-    data = aiohttp.FormData()
-    data.add_field("url", url)
-
-    async with aiohttp.ClientSession() as session:
-        async with session.post("https://git.io", data=data) as resp:
-            if resp.status == 201 and "Location" in resp.headers:
-                return resp.headers["Location"]
-
     return url
